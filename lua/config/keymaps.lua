f931c9815163b7fe32f37d2d277effd0f85371ac
Martin Vala <vala.martin@gmail.com>
2024-10-02 14:26:45 +0200

    Switch keymaps for buffer next

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-map("n", "<tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-map("n", "<S-tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<S-tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
