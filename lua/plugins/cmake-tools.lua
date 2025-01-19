e0b51f3a2cd661cfd2fddb3be72d7d6341f747a4
Martin Vala <vala.martin@gmail.com>
2025-01-19 20:06:31 +0100

    Update

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -5,6 +5,8 @@ return {
     keys = {
       { "<leader>cb", "<cmd>CMakeBuild<cr>", desc = "CMake build", mode="n" },
       { "<leader>ci", "<cmd>CMakeInstall<cr>", desc = "CMake install", mode="n" },
+      { "<F2>", "<cmd>CMakeBuild<cr>", desc = "CMake build", mode="n" },
+      { "<F3>", "<cmd>CMakeInstall<cr>", desc = "CMake install", mode="n" },
     },
   },
 }
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -42,6 +42,23 @@ return {
         },
       },
     },
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        -- dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        -- dapui.close()
+      end
+    end,
   },
   {
     "mfussenegger/nvim-dap",
